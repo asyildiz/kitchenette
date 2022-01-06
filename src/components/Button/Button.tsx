d5df5c9cca9edc22f3a1e4427dda4636b06d528a
Akif Safa Yıldız <akifsafayildiz@gmail.com>
2022-01-06 15:25:27 +0300

    add: color palette

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,25 @@
 import React from "react";
+import classNames from 'classnames/bind';
 import './Button.scss';
 
 interface ButtonProps {
   label?: string;
   disabled?: boolean;
-  type?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link',
+  type?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'link';
+  fullWidth?: boolean;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button className={props.type} disabled={props.disabled}>{props.label}</button>
+    let buttonClass = classNames(props.type, { 'disabled': props.disabled, 'full-width': props.fullWidth });
+
+    return <button className={buttonClass} disabled={props.disabled}>{props.label}</button>
 }
 
 Button.defaultProps = {
     label: 'Lorem ipsum',
     disabled: false,
     type: 'primary',
+    fullWidth: false
 }
 
 export default Button;
